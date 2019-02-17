398ce740f8273a4f5441dc694c88c489da35f33b
SummonerL <elliotsminion@gmail.com>
2019-02-16 20:59:17 -0500

    finished rates table

--- assets/js/utilities.js
+++ assets/js/utilities.js
@@ -1,7 +1,34 @@
+// let's pull in the json on page load
+$(document).ready(function() {
+    var rates = [];
+    $.getJSON( "https://raw.githubusercontent.com/allylabs/fed-coding-challenge/public/code-test.json", function( json ) {
+        rates = rates.concat(json);
+
+        constructTable(rates);
+    });
+});
+
 function openNav() {
     $('#primary-nav').toggleClass('nav__items-open');
 };
 
 function toggleModal(modalName) {
     $('#'+modalName).toggleClass('modal-open');
+}
+
+function constructTable(rates) {
+    // sort rates by earnings
+    rates = rates.sort(function(rate1, rate2) {return rate2.earnings - rate1.earnings})
+
+    rates.forEach(function (rate) {
+        $('#ratesTable tbody').append(
+            '<tr><td>' +
+            rate.name +
+            '</td><td>' +
+            rate.apy +
+            ' %</td><td>$' +
+            (rate.earnings).toFixed(2) +
+            '</td></tr>'
+        );
+    });
 }
